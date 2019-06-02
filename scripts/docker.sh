7d6f6ab57f4575d9b774d8fede04808fcb1ffc4e
AlejandroGarcia95 <alemgarcia95@gmail.com>
2019-06-02 09:26:54 -0700

    Level 3 with a cipher

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -5,7 +5,7 @@ set -exuo
 # Install Docker CE
 ## Set up the repository:
 ### Install packages to allow apt to use a repository over HTTPS
-apt-get update && apt-get install apt-transport-https ca-certificates curl software-properties-common
+apt-get update && apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 
 ### Add Docker’s official GPG key
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
