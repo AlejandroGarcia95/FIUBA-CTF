a4007c4e503e7c372ed11d878a7dffee825279c2
AlejandroGarcia95 <alemgarcia95@gmail.com>
2019-06-15 15:55:37 -0700

    Formatted page in a nice manner, everything done

--- levels/level7/penlab/comment.php
+++ levels/level7/penlab/comment.php
@@ -6,26 +6,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-    <title>Pentesting Lab</title>
+    <title>Vapour Community</title>
 
     <!-- Bootstrap core CSS -->
     <link href="/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
     <link href="/style.css" rel="stylesheet">
-
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
   </head>
 
   <body>
-
     <div class="container">
-        <h1>Widgets.Co</h1>
-        <h4>Please Leave us a comment</h4>
+        <h1>Age of Mythology: Extended Edition</h1>
+        <h4>Please leave us your review</h4>
         <form action="save.php" method="POST">
             <div class="form-group">
                 <textarea name="comment" id="comment" rows="5" class="form-control"></textarea>
@@ -35,7 +28,7 @@
             </div>
         </form>
         </br>
-        <a href="view.php">View Latest Comment</a>
+        <a href="view.php">View latest reviews</a>
     </div>
   </body>
 </html>
--- levels/level7/penlab/save.php
+++ levels/level7/penlab/save.php
@@ -16,28 +16,21 @@ if (isset($_POST["comment"])) {
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-    <title>Pentesting Lab</title>
+    <title>Vapour Community</title>
 
     <!-- Bootstrap core CSS -->
     <link href="/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
     <link href="/style.css" rel="stylesheet">
-
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
   </head>
 
   <body>
-
     <div class="container">
-        <h1>Widgets.Co</h1>
-        <h4>Your Comment Has Been Saved</h4>
+        <h1>Age of Mythology: Extended Edition</h1>
+        <h4>Your review has been saved!</h4>
         <hr/>
-        <a href="view.php">View Latest Comment</a>
+        <a href="view.php">View latest reviews</a>
     </div>
   </body>
 </html>
